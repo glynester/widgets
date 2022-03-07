0c73b49dd75d802c433ee060f7a8b020a7e06dc0
Gee <glynester@hotmail.com>
2022-03-07 16:39:41 +0000

    Set up useState hook to determine clicked item index. Toggled className.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion = ({items})=>{
-  console.log(items);
-  const renderedItems=items.map(item=>{
+  // Array destructuring
+  console.log("XXXX",useState(null))
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick=(index)=>{
+    // console.log('Title clicked',index);
+    setActiveIndex(index);
+  }
+
+  
+  const renderedItems=items.map((item,index)=>{
+    // active - will add a cLassName 'active' to show or add '' to hide the detail of the item as appropriate.
+    const active = index===activeIndex?'active':'';
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className={`title ${active}`} onClick={()=>onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
@@ -19,6 +30,7 @@ const Accordion = ({items})=>{
   // return <h1>{items.length}</h1>;
   return <div className="ui styled accordion">
       {renderedItems}
+      {/* <h1>{activeIndex}</h1> */}
     </div>;
 }
 
