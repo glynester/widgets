e69835105c38ac2c33f036dd9aef821492582134
Gee <glynester@hotmail.com>
2022-03-08 20:28:37 +0000

    API call to wiki done with axios. Results display now using useState to store 'term' and 'results'.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search=()=>{
-  const [term, setTerm]=useState('');
-
+  const [term, setTerm]=useState('programming');
+  const [results, setResults]=useState([]);
+  // console.log("This runs with every term render");
+  useEffect(()=>{
+    // console.log("All renders");
+   const search=async()=>{
+    const { data } = await axios.get('https://en.wikipedia.org/w/api.php',{
+      params: {
+        action: "query",
+        list: "search",
+        format: "json",
+        origin: "*",
+        srsearch: term
+      }
+    });
+    setResults(data.query.search);
+    console.log("result=>",results);
+   }
+  //  if (term){ search(); }  // Only search if not blank.
+  search()
+  },[term])
   const updateTerm=(value)=>{
     setTerm(value);
-    console.log("term",term);
+    // console.log("term",term);
   }
-
+  const renderedResults=results.map(result=>{
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    )
+  })
   return (
     <div>
       Search component
@@ -20,6 +49,9 @@ const Search=()=>{
           />
         </div>
       </div>
+
+      <div className="ui celled list">{renderedResults}</div>
+
     </div>
   )
 }
