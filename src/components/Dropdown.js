a15dbadde9b2c1e767e0ed00f4d6c27365cfe1f8
Gee <glynester@hotmail.com>
2022-03-11 15:29:20 +0000

    useEffect hook and useRef to capture click outside of Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown=({options, onSelectedChange, selected})=>{
   const [open, setOpen]=useState(false);
+  const ref = useRef();
+
+  useEffect(()=>{
+    document.body.addEventListener('click',(event)=>{
+      // console.log(event.target);
+      // console.log('Body click!!!');
+      if (ref.current.contains(event.target)){
+        return;
+      }
+      setOpen(false);
+    },{ capture: true })
+  },[]);    // [] = only want this to run once.
 
   const renderedOptions=options.map(option=>{
     if (option.value === selected.value) { return null; }
@@ -9,19 +21,26 @@ const Dropdown=({options, onSelectedChange, selected})=>{
       <div 
         key={option.value} 
         className="item"
-        onClick={()=>{onSelectedChange(option)}} 
+        onClick={()=>{
+          // console.log('Item click!!!');
+          onSelectedChange(option)
+        }} 
       >
         {option.label}
       </div>
     )
   })
+  // console.log("ref",ref.current);
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">
           Select a colour
         </label>
-        <div onClick={()=>setOpen(!open)} className={`ui selection dropdown ${open?'visible active':''}`}>
+        <div onClick={()=>{
+            // console.log('Dropdown click!!!');
+            setOpen(!open);
+          }} className={`ui selection dropdown ${open?'visible active':''}`}>
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
           <div className={`menu ${open?'visible transition':''}`}>
