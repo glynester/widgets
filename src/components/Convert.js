8ec0ad14e6fc0987effe022cb2d9a1cc33076013
Gee <glynester@hotmail.com>
2022-03-14 20:11:15 +0000

    Code in useEffect to debounce the API call

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,13 +4,25 @@ import axios from 'axios'
 const Convert=({language, text})=>{
   console.log("API==>",process.env.NODE_ENV);
   const [translated, setTranslated]=useState('');
+  const [debouncedText, setDebouncedText]=useState(text);
+
+  useEffect(()=>{
+    const timerId = setTimeout(()=>{
+      setDebouncedText(text);
+    },500);
+    // cleanup function.
+    return ()=>{
+      clearTimeout(timerId);
+    }
+  },[text]);
+
   useEffect(()=>{
     // console.log("New language/text");
-    // 2nd arg emty coz nothing to be sent in body
+    // 2nd arg empty coz nothing to be sent in body
     const doTranslation=async()=>{
       const res=await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
       params: {
-        q: text,
+        q: debouncedText,   // change from 'text'
         target: language.value, 
         key: process.env.REACT_APP_GOOGLE_TRANSLATE_API
       } 
@@ -18,7 +30,7 @@ const Convert=({language, text})=>{
     setTranslated(res.data.data.translations[0].translatedText)
     }
     doTranslation();
-  },[language,text]);
+  },[language,debouncedText]); // change from 'text'
 
   return (
     <div>
