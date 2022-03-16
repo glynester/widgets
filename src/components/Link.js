7e244291567701670d0cb36244b78ab311bad10c
Gee <glynester@hotmail.com>
2022-03-16 14:55:15 +0000

    Navigation completed without using React Router

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,23 @@
-import React from 'React';
+import React from 'react';
 
-const Link=()=>{
+const Link=({className, href, children})=>{
+  // helper function
+  const onClick=(event)=>{
+    // Makes a link open in a new tab when CTRL (windows) + link clicked
+    if (event.metaKey || event.ctrlKey){
+      return;
+    }
+    event.preventDefault(); // Stops page reloading
+    window.history.pushState({},'',href);   // Updates the URL
+    const navEvent = new PopStateEvent('popstate'); // Tell  route components that data (the URL) has just changed.
+    window.dispatchEvent(navEvent);
+
+  }
+
   return (
-    <a>Link</a>
+    <a onClick={onClick} className={className} href={href}>
+      {children}
+    </a>
   )
 }
 
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,8 +1,28 @@
 // import React from 'react';  // No JSX in file so not needed.
+import { useEffect, useState } from 'react';
 
 // A component (JSX tag) passed within another component is identified as a prop called 'children' to the outer component.
 const Route=({ path, children })=>{
-  return window.location.pathname===path?children : null;
+  const [currentPath, setCurrentPath]=useState(window.location.pathname);
+  useEffect(()=>{
+    // Defined separately so it can be removed later on.
+    const onLocationChange=()=>{
+      // console.log("Location changed!!!");
+      // Changing this, get the Route to update
+      setCurrentPath(window.location.pathname);
+    }
+    window.addEventListener('popstate', onLocationChange);
+    // Cleanup function
+    return ()=>{
+      window.removeEventListener('popstate', onLocationChange);
+    }
+
+  },[]); // Only want it to run once (when first rendered to screen), hence '[]' argument.
+
+  // currentPath = window.location.pathname so could use either
+  return currentPath === path?children : null;
 }
 
 export default Route;
+
+// Listen for event despatched using useState.
