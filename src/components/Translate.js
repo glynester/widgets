69ddc5b06b255dad7e725f136ef9259a0996f5ab
Gee <glynester@hotmail.com>
2022-03-16 16:12:00 +0000

    Note to show Google translate only works from localhost

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -9,7 +9,7 @@ const Translate = () => {
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Enter Text</label>
+          <label>Enter Text { window.location.hostname!=="localhost"?"(NOTE - The google translate API only works from 'localhost')":''}</label>
           <input value={text} onChange={ (e)=>setText(e.target.value) } />
         </div>
       </div>
